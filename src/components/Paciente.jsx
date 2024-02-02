6cc5d29a3597c0cc4d0de4c3b18db38bd91db663
Daniel Leon <danielmatus468@gmail.com>
2024-02-02 17:05:28 -0600

    Completed basic design for Pacientes, Listado and Formulario components / Added Tailwindcss

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,21 @@
+export const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Daniel</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: {''}
+                <span className="font-normal normal-case">02/02/2024</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorum ex itaque minus nostrum quas quos sed sequi sint ullam. Accusamus adipisci beatae consequatur corporis cupiditate deleniti dolor earum eligendi error explicabo fugit id magni necessitatibus non nulla numquam quaerat quasi quod, rem rerum tempora tenetur ut. Illo necessitatibus, obcaecati.</span>
+            </p>
+        </div>
+    )
+}
